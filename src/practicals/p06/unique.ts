14af9f7c29b8b09b114b3f44f8744a21eb6bcd80
PrakritPrad <poom66310@gmail.com>
2026-01-21 16:06:20 +0700

    uiqnum2

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,9 +1,9 @@
 export function getUniqueNumbers(arr1: number[], arr2: number[]) {
   const newArray: number[] = arr1.concat(arr2);
   const unique = [...new Set(newArray)]
-  console.log(unique)
+  return unique
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
-getUniqueNumbers(arr1,arr2);
+console.log(getUniqueNumbers(arr1,arr2))
