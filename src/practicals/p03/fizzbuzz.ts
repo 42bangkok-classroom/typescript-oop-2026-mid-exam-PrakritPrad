10995df99cb13b20c3cea1c20e4b1be1f6d11c07
PrakritPrad <poom66310@gmail.com>
2026-01-21 14:34:56 +0700

    fizzbuzzzzzz

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,19 @@
 // Write your code below
+const n: number = parseFloat(process.argv[2]);
+if(isNaN(n)|| n < 1 ||!Number.isInteger(n)){
+
+}
+else{
+    for(let i = 1;i<=n;i++){
+        if(i % 3 ===0 && i % 5 ===0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0){
+            console.log('Fizz');
+        }else if(i % 5 === 0){
+            console.log('Buzz');
+        }
+        else{
+            console.log(`${i}`);
+        }
+        }
+    }
