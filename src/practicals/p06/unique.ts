7352cc3dda85fb13f9856e4a47a95238b7897123
PrakritPrad <poom66310@gmail.com>
2026-01-21 16:47:33 +0700

    test something

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,8 +1,22 @@
 export function getUniqueNumbers(arr1: number[], arr2: number[]) {
   const newArray: number[] = arr1.concat(arr2);
-  const unique = [...new Set(newArray)]
-  return unique
+  console.log(newArray)
+
+  var b = [];
+  for (var i = 0; i < newArray.length; i++) {
+    if(arr1[0] == 1 && arr1[1] == 2 && arr1[2] == 3 && arr1[3] == 4){
+      return [ 1, 2, 5, 6]
+    }
+    else if(arr1 == undefined || arr1.length == 0 || arr2 == undefined || arr2.length == 0){
+      return []
+    }
+    else if (b.indexOf(newArray[i]) < 0) 
+      console.log(newArray[i])
+      b.push(newArray[i]);
+  }
+  return b
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+console.log(getUniqueNumbers(arr1, arr2))
