5bef36ce44de03da97adf875cb7e8a56969c9d6e
PrakritPrad <poom66310@gmail.com>
2026-01-21 16:03:41 +0700

    test uniq

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,9 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+export function getUniqueNumbers(arr1: number[], arr2: number[]) {
+  const newArray: number[] = arr1.concat(arr2);
+  const unique = [...new Set(newArray)]
+  console.log(unique)
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+getUniqueNumbers(arr1,arr2);
--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,3 +1,19 @@
-function readJson(fileName: string): unknown[] {
-  
+import * as fs from 'node:fs/promises';
+
+export async function readJson(fileName: string): Promise<unknow[]>{
+    try{
+        const fileContent = await fs.readFile(fileName, 'utf-8');
+        const carts = JSON.parse(fileContent);
+        return carts;
+    }
+    catch(error){
+        if(error instanceof SyntaxError){
+            console.error("Invalis JSON format");
+        }
+        else{
+            console.error("Invalid file")
+        }
+        return[];
+    }
 }
+readJson("users.json")
