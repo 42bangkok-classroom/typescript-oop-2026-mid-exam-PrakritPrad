121edd1496fdcec999473957f4db0c297b7dee3a
PrakritPrad <poom66310@gmail.com>
2026-01-21 14:29:07 +0700

    p02andp01 test

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,29 @@
 // Write your code below
+let Input1 = Number(process.argv[2]);
+
+
+
+if(Number.isNaN(Input1)||(Input1 < 0) || (Input1 > 100) || process.argv[2] === '' || !process.argv[2] || process.argv[2] === " " ){
+   console.log("Invalid input");
+   process.exit();  
+}
+
+if(Input1 >= 50 && Input1 < 60){
+  console.log("Grade is D");
+}
+
+else if(Input1 >= 60 && Input1 < 70){
+  console.log("Grade is C");
+}
+
+else if(Input1 >= 70 && Input1 < 80){
+  console.log("Grade is B");
+}
+
+else if(Input1 >= 80 && Input1 <= 100){
+  console.log("Grade is A");
+}
+
+else if (Input1 >= 0 && Input1 < 50){
+  console.log("Grade is F");
+}
